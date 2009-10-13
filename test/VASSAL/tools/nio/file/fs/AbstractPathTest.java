0bb5e883415ddb79d5088d3dcf2e519e068c81d4
cfedeli <cfedeli@mycompany.com>
2009-10-13 10:10:28 +0000

    Refactored the tests: migrated most of the tests in RealPath.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/branches/nio-fs@6144 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- test/VASSAL/tools/nio/file/fs/AbstractPathTest.java
+++ test/VASSAL/tools/nio/file/fs/AbstractPathTest.java
@@ -2,29 +2,9 @@ package VASSAL.tools.nio.file.fs;
 
 import java.io.IOException;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public abstract class AbstractPathTest {
-
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-  }
-
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {
-  }
-
-  @Before
-  public void setUp() throws Exception {
-  }
-
-  @After
-  public void tearDown() throws Exception {
-  }
 
   @Test
   public abstract void testCompareTo();
