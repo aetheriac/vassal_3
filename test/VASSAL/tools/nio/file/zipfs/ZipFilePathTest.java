e2dcc25ae12ce0cb3a4fa884ed0992d40d1cbff3
uckelman <uckelman@mycompany.com>
2009-11-26 23:33:03 +0000

    Ported parametrized RealUnixPath tests to zipfs.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/branches/nio-fs@6301 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ test/VASSAL/tools/nio/file/zipfs/ZipFilePathTest.java
@@ -0,0 +1,281 @@
+package VASSAL.tools.nio.file.zipfs;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import VASSAL.tools.nio.file.FileSystem;
+import VASSAL.tools.nio.file.FileSystems;
+import VASSAL.tools.nio.file.Path;
+import VASSAL.tools.nio.file.Paths;
+import VASSAL.tools.nio.file.PathEndsWithTest;
+import VASSAL.tools.nio.file.PathGetNameIntTest;
+import VASSAL.tools.nio.file.PathIsAbsoluteTest;
+import VASSAL.tools.nio.file.PathNormalizationTest;
+import VASSAL.tools.nio.file.PathNormalizeTest;
+import VASSAL.tools.nio.file.PathRelativizeTest;
+import VASSAL.tools.nio.file.PathResolveTest;
+import VASSAL.tools.nio.file.PathStartsWithTest;
+import VASSAL.tools.nio.file.PathSubpathTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+  ZipFilePathTest.EndsWithTest.class,
+  ZipFilePathTest.GetNameIntTest.class,
+  ZipFilePathTest.IsAbsoluteTest.class,
+//  ZipFilePathTest.NormalizationTest.class,
+  ZipFilePathTest.NormalizeTest.class,
+  ZipFilePathTest.RelativizeTest.class,
+  ZipFilePathTest.ResolveTest.class,
+  ZipFilePathTest.StartsWithTest.class,
+  ZipFilePathTest.SubpathTest.class
+})
+public class ZipFilePathTest {
+  protected static FileSystem fs;
+
+  protected static final String zfName = "testZipFile.zip";
+  protected static final String zfPathName =
+    "test/VASSAL/tools/nio/file/zipfs/".replace("/", File.separator) + zfName;
+
+  protected static Path zfPath;
+
+  @BeforeClass
+  public static void setupFS() throws IOException {
+    zfPath = Paths.get(zfPathName).toAbsolutePath();
+
+    fs = (ZipFileSystem) FileSystems.newFileSystem(
+      URI.create("zip://" + zfPath), null);
+  }
+
+  @RunWith(Parameterized.class)
+  public static class EndsWithTest extends PathEndsWithTest {
+    public EndsWithTest(String left, String right, boolean expected) {
+      super(ZipFilePathTest.fs, left, right, expected);
+    }
+
+    @Parameters
+    public static List<Object[]> cases() {
+      return Arrays.asList(new Object[][] {
+        // Left       Right       Expected
+        { "/",        "/",        true  },
+        { "/",        "foo",      false },
+        { "/",        "/foo",     false },
+        { "/foo",     "foo",      true  },
+        { "/foo",     "/foo",     true  },
+        { "/foo",     "/",        false },
+        { "/foo/bar", "bar",      true  },
+        { "/foo/bar", "foo/bar",  true  },
+        { "/foo/bar", "/foo/bar", true  },
+        { "/foo/bar", "/bar",     false },
+        { "foo",      "foo",      true  },
+        { "foo/bar",  "bar",      true  },
+        { "foo/bar",  "foo/bar",  true  }
+      });
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class GetNameIntTest extends PathGetNameIntTest{
+    public GetNameIntTest(String input, int index,
+                          String expected, Class<? extends Throwable> tclass) {
+      super(ZipFilePathTest.fs, input, index, expected, tclass);
+    }
+
+    @Parameters
+    public static List<Object[]> cases() {
+      return Arrays.asList(new Object[][] {
+        // Input   Index Expected Throws
+        { "a/b/c",  -1,  null,    IllegalArgumentException.class }, 
+        { "a/b/c",   0,  "a",     null                           },
+        { "a/b/c",   1,  "b",     null                           },
+        { "a/b/c",   2,  "c",     null                           },
+        { "a/b/c",   3,  null,    IllegalArgumentException.class },
+        { "/a/b/c", -1,  null,    IllegalArgumentException.class }, 
+        { "/a/b/c",  0,  "a",     null                           },
+        { "/a/b/c",  1,  "b",     null                           },
+        { "/a/b/c",  2,  "c",     null                           },
+        { "/a/b/c",  3,  null,    IllegalArgumentException.class },
+        { "/",       0,  null,    IllegalArgumentException.class }
+      });
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class IsAbsoluteTest extends PathIsAbsoluteTest {
+    public IsAbsoluteTest(String input, boolean expected) {
+      super(ZipFilePathTest.fs, input, expected);
+    }
+
+    @Parameters
+    public static List<Object[]> cases() {
+      return Arrays.asList(new Object[][] {
+        // Input  Expected
+        { "/",    true  },
+        { "/tmp", true  },
+        { "tmp",  false }
+      });
+    }
+  }
+
+/*
+  @RunWith(Parameterized.class)
+  public static class NormalizationTest extends PathNormalizationTest{
+    public NormalizationTest(String input, String expected) {
+      super(ZipFilePathTest.fs, input, expected);
+    }
+
+    @Parameters
+    public static List<String[]> cases() {
+      return Arrays.asList(new String[][] {
+        // Input                Expected
+        { "/.",            "/"       },
+        { "//foo",         "/foo"    },
+        { "/foo//",        "/foo"    },
+        { "/foo/./",       "/foo"    },
+        { "/foo/./.././.", "/foo/.." },
+        { ".",             null      },
+        { "foo",           "foo"     },
+        { "foo//",         "foo"     },
+        { "foo/./",        "foo"     },
+        { "foo/./.././.",  "foo/.."  }
+      });
+    }
+  }
+*/
+
+  @RunWith(Parameterized.class)
+  public static class NormalizeTest extends PathNormalizeTest{
+    public NormalizeTest(String input, String expected) {
+      super(ZipFilePathTest.fs, input, expected);
+    }
+
+    @Parameters
+    public static List<String[]> cases() {
+      return Arrays.asList(new String[][] {
+        // Input                Expected
+        { "/",                  "/"         },
+        { "foo",                "foo"       },
+        { ".",                  null        },
+        { "..",                 ".."        },
+        { "/..",                "/"         },
+        { "/../..",             "/"         },
+        { "foo/.",              "foo"       },
+        { "./foo",              "foo"       },
+        { "foo/..",             null        },
+        { "../foo",             "../foo"    },
+        { "../../foo",          "../../foo" },
+        { "foo/bar/..",         "foo"       },
+        { "foo/bar/baz/../..",  "foo"       },
+        { "/foo/bar/baz/../..", "/foo"      }
+      });
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class RelativizeTest extends PathRelativizeTest {
+    public RelativizeTest(String left, String right, String expected) {
+      super(ZipFilePathTest.fs, left, right, expected);
+    }
+
+    @Parameters
+    public static List<String[]> cases() {
+      return Arrays.asList(new String[][] {
+        // Left     Right      Expected
+        { "/a/b/c", "/a/b/c",  null         },
+        { "/a/b/c", "d/e",     "/a/b/c/d/e" },
+        { "/a/b/c", "../../x", "/a/x"       }
+      });
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class ResolveTest extends PathResolveTest {
+    public ResolveTest(String left, String right, String expected) {
+      super(ZipFilePathTest.fs, left, right, expected);
+    }
+
+    @Parameters
+    public static List<String[]> cases() {
+      return Arrays.asList(new String[][] {
+        // Left   Right   Expected
+        { "/tmp", "foo",  "/tmp/foo" },
+        { "/tmp", "/foo", "/foo"     },
+        { "tmp",  "foo",  "tmp/foo"  },
+        { "tmp",  "/foo", "/foo"     }
+      });
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class StartsWithTest extends PathStartsWithTest{
+    public StartsWithTest(String left, String right, boolean expected) {
+      super(ZipFilePathTest.fs, left, right, expected);
+    }
+
+    @Parameters
+    public static List<Object[]> cases() {
+      return Arrays.asList(new Object[][] {
+        // Left       Right       Expected
+        { "/",        "/",        true  },
+        { "/",        "/foo",     false },
+        { "/foo",     "/",        true  },
+        { "/foo",     "/foo",     true  },
+        { "/foo",     "/f",       false },
+        { "/foo/bar", "/",        true  },
+        { "/foo/bar", "/foo",     true  },
+        { "/foo/bar", "/foo/bar", true  },
+        { "/foo/bar", "/f",       false },
+        { "/foo/bar", "foo",      false },
+        { "/foo/bar", "foo/bar",  false },
+        { "foo",      "foo",      true  },
+        { "foo",      "f",        false },
+        { "foo/bar",  "foo",      true  },
+        { "foo/bar",  "foo/bar",  true  },
+        { "foo/bar",  "f",        false },
+        { "foo/bar",  "/foo",     false },
+        { "foo/bar",  "/foo/bar", false },
+      });
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class SubpathTest extends PathSubpathTest{
+    public SubpathTest(String input, int begin, int end, String expected,
+                                           Class<? extends Throwable> tclass) {
+      super(ZipFilePathTest.fs, input, begin, end, expected, tclass);
+    }
+
+    @Parameters
+    public static List<Object[]> cases() {
+      return Arrays.asList(new Object[][] {
+        // Input       Begin End Expected   Throws 
+        { "/",             0, 1, null,      IllegalArgumentException.class },
+        { "/foo/bar/baz", -1, 0, null,      IllegalArgumentException.class },
+        { "/foo/bar/baz",  0, 1, "foo",     null                           },
+        { "/foo/bar/baz",  0, 2, "foo/bar", null                           },
+        { "/foo/bar/baz",  0, 3, "foo/bar/baz", null                       },
+        { "/foo/bar/baz",  1, 2, "bar",     null                           },
+        { "/foo/bar/baz",  1, 3, "bar/baz", null                           },
+        { "/foo/bar/baz",  2, 3, "baz",     null                           },
+        { "/foo/bar/baz",  1, 0, null,      IllegalArgumentException.class },
+        { "foo/bar/baz",  -1, 0, null,      IllegalArgumentException.class },
+        { "foo/bar/baz",   0, 1, "foo",     null                           },
+        { "foo/bar/baz",   0, 2, "foo/bar", null                           },
+        { "foo/bar/baz",   0, 3, "foo/bar/baz", null                       },
+        { "foo/bar/baz",   1, 2, "bar",     null                           },
+        { "foo/bar/baz",   1, 3, "bar/baz", null                           },
+        { "foo/bar/baz",   2, 3, "baz",     null                           },
+        { "foo/bar/baz",   1, 0, null,      IllegalArgumentException.class }
+      });
+    }
+  }
+}  
