986324e92c245bfcb45a38cfe51c253e4bd266ac
uckelman <uckelman@mycompany.com>
2010-01-31 22:48:29 +0000

    Cleanup.
    git-svn-id: https://svn.code.sf.net/p/vassalengine/svn/VASSAL-src/branches/nio-fs@6445 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- test/VASSAL/tools/nio/file/PathGetAttributeTest.java
+++ test/VASSAL/tools/nio/file/PathGetAttributeTest.java
@@ -9,18 +9,18 @@ import org.junit.Test;
 
 public abstract class PathGetAttributeTest extends AbstractPathMethodTest {
   protected final String input;
-  protected final String attrib;
+  protected final String attr;
 
   public PathGetAttributeTest(FSHandler fac, String input,
-                              String attrib, Object expected) {
+                              String attr, Object expected) {
     super(fac, expected);
 
     this.input = input;
-    this.attrib = attrib;
+    this.attr = attr;
   }
 
   protected void doTest() throws IOException {
-    final Object result = fs.getPath(input).getAttribute(attrib);
+    final Object result = fs.getPath(input).getAttribute(attr);
     
     if (expected instanceof byte[]) {
       assertArrayEquals((byte[]) expected, (byte[]) result);
